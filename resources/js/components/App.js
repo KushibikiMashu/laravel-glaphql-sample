31d4358cbb1a5a2449333ae632f043772e2ebe8c
KushibikiMashu <kushibiki3413@gmail.com>
2019-06-19 10:23:54 +0900

    apollo-clientの準備

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Example from './Example'
+import User from './User'
 
 const App = () => {
     return (
         <>
             <Example/>
-            {/*<User />*/}
+            <User />
         </>
     )
 }
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, {gql} from 'apollo-boost'
 import App from './App'
+
+const client = new ApolloClient({
+    uri: "http://127.0.0.1:8001/graphql"
+})
+
+client
+    .query({
+        query: gql`
+            {
+              users {
+                id
+                name
+                email
+              }
+            }
+        `
+    })
 
 ReactDOM.render(
     <App />,
