e32f6010aaa8b870088dff1504c213ce503b9828
KushibikiMashu <kushibiki3413@gmail.com>
2019-06-20 08:40:55 +0900

    エラーハンドリングを追加

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ApolloClient, {gql} from 'apollo-boost'
+import ApolloClient, { gql } from 'apollo-boost'
 import { ApolloProvider } from "react-apollo";
 import User from './User'
 import Error from "./Error";
 
 const client = new ApolloClient({
-    uri: "http://127.0.0.1:8001/graphql"
+    uri: "http://127.0.0.1:8001/graphql",
+    onError: ({graphQLErrors, networkError}) => {
+        if (graphQLErrors) {
+            graphQLErrors.map(({message, locations, path}) =>
+                console.log(
+                    `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                ),
+            );
+        }
+
+        if (networkError) {
+            console.log(`[Network error]: ${networkError}`);
+        }
+    }
 })
 
 const App = () => (
     <ApolloProvider client={client}>
         <User/>
-        <Error />
+        <Error/>
     </ApolloProvider>
 )
 
 render(
-    <App />,
+    <App/>,
     document.getElementById('root')
 )
