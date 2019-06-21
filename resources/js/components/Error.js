13a0a196c5f72cc95de83a129c3fef10ce2f6c37
KushibikiMashu <kushibiki3413@gmail.com>
2019-06-21 09:10:27 +0900

    Error Messageをボタンで表示/非表示を切り替える

--- resources/js/components/Error.js
+++ resources/js/components/Error.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
@@ -8,19 +8,27 @@ const query = gql`
     }
 `
 
-const Error = () => (
-    <Query query={query}>
-        {({loading, error, data}) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <>
-                <hr />
-                <h2>Error Message</h2>
-                <p>{error.toString()}</p>
-            </>;
+const Error = () => {
+    const [isShown, toggle] = useState(false);
 
-            return <>Error Component</>
-        }}
-    </Query>
-);
+    return (
+        <Query query={query}>
+            {({loading, error, data}) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return (
+                    <div style={{margin: 60}}>
+                        <hr/>
+                        <button type="button" onClick={() => toggle(!isShown)}>Query Error Message</button>
+                        <div style={{display: isShown ? 'block' : 'none'}}>
+                            <h2>Error Message</h2>
+                            <p>{error.toString()}</p>
+                        </div>
+                    </div>);
+
+                return <>Error Component</>
+            }}
+        </Query>
+    );
+}
 
 export default Error;
